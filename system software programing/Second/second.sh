181bb910aefbc0c3a9692c482963eaae940531ca
zawgy <legenda2121@mail.ru>
2018-11-08 12:45:56 +0300

    Fix second task

--- system software programing/Second/second.sh
+++ system software programing/Second/second.sh
@@ -1,4 +1,4 @@
 #!/bin/ksh
 
-getent passwd | cut -f 1 -d ':' | xargs groups | egrep " $1( |$)" | cut -f 1 -d ':' | sort
+getent passwd | nawk -F: '{print $1}' | xargs groups | egrep "\<$1\>" | nawk -F: '{print $1}' | sort
 
